6af9b691a2b916e8cf9b25c45b4aa144f085d89e
naidu28 <snaidu1228@gmail.com>
2017-01-24 17:35:31 -0500

    some stuff

--- scripts/components/contact-form.js
+++ scripts/components/contact-form.js
@@ -30,25 +30,30 @@ function contactForm() {
    var formWrapper = "<div id='contact-form-wrapper' class='lato'><form></form></div>";
    //var colOne = "<div class='column'></div>";
    var colTwo = "<div class='column'></div>";
-   var inputName = "<label for='name'>Name</label><input class='form-text' name='name' type='text' />";
+   /*var inputName = "<label for='name'>Name</label><input class='form-text' name='name' type='text' />";
    var inputEmail = "<label for='email'>Email</label><input class='form-text' name='email' type='text' />";
    var inputSubject = "<label for='subject'>Subject</label><input class='form-text' name='subject' type='text' />";
    var inputMessage = "<label for='message'>Message</label><textarea class='form-message' cols='60' name='message' rows='8'></textarea>";
-   var submit = "<div name='submit' class='form-submit lato' onclick='replaceWithSpinner()'><span id='contact-form-submit'>Submit</span></div>";
+   var submit = "<div name='submit' class='form-submit lato' onclick='replaceWithSpinner()'><span id='contact-form-submit'>Submit</span></div>";*/
 
+   var form = '<form class="column" method="POST" action="http://formspree.io/snaidu1228@gmail.com"></form>';
+   var email = '<input class="form-text" type="email" name="email" placeholder="Your email">';
+   var text = '<textarea class="form-message" name="message" placeholder="Your message" cols="60" rows="8"></textarea>';
+   var submit = '<div name="submit" class="form-submit lato" onclick="replaceWithSpinner();" type="submit"><span id="contact-form-submit">Send</span></div>'
+
+   form = $(form)
+      .append(email)
+      .append(text)
+      .append(submit);
    /*colOne = $(colOne)
      .append(inputName)
      .append(inputEmail);*/
 
-   colTwo = $(colTwo)
-     .append(inputName)
-     .append(inputEmail)
-     .append(inputSubject)
-     .append(inputMessage)
-     .append(submit);
+   /*colTwo = $(colTwo)
+     .append(form);*/
 
    formWrapper = $(formWrapper)
-     .append(colTwo);
+     .append(form);
 
    content = $(content)
      .append(formWrapper);
